95ba958ee3ca93b04c3d1bd9f1715e53962d31e4
ShitalBhise <bhiseshital27059@gmail.com>
2022-03-19 00:06:21 +0530

    Null mood return Happy

--- java/MoodAnalyzerMain.java
+++ java/MoodAnalyzerMain.java
@@ -19,5 +19,22 @@ public class MoodAnalyzerMain {
 
 		Assert.assertEquals("HAPPY", result);
 	}
+	@Test
+	public  anymood_should returnHappy() {
+		moodanalyzer.setMessage("I am in Any Mood");
+		String result = moodanalyzer.analyseMood();
+		Assert.assertEquals("HAPPY", result);
+	}
+
+	/**
+	 * we have created this method for null exception handle Null Mood Should Return
+	 * Happy
+	 */
+	@Test
+	public void NullexceptionHadle() {
+		moodanalyzer.setMessage(null);
+		String result = moodanalyzer.analyseMood();
+		Assert.assertEquals("Happy", result);
+	}
 
 }
--- java/MoodAnalyzerTest.java
+++ java/MoodAnalyzerTest.java
@@ -4,17 +4,42 @@ public class MoodAnalyzerTest {
 	 * we are converting the passing message to lower case and checking the contains
 	 * happy or sad word and if happy and sad present then we can return values
 	 */
-	public static String analyseMood(String message) {
+	String message;;
 
-		if (message.toLowerCase().contains("happy")) {
-			return "HAPPY";
-		} else if (message.toLowerCase().contains("sad")) {
-			return "SAD";
-		} else if (message.toLowerCase().contains("any")) {
-			return "HAPPY";
-		} else {
-			return "Null";
+	/* getter setter created for string Message */
+	public String getMessage() {
+		return message;
+	}
 
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/* default constructor */
+	public MoodAnalyzer() {
+
+	}
+
+	/* Parameterized constructor */
+	public void MoodAnalyzer(String message) {
+		this.message = message;
+
+	}
+	
+	public String analyseMood() {
+
+		try {
+			if (message.toLowerCase().contains("happy")) {
+				return "HAPPY";
+			} else if (message.toLowerCase().contains("sad")) {
+				return "SAD";
+			} else if (message.toLowerCase().contains("any")) {
+				return "HAPPY";
+			} else
+				return "ANY";
+
+		} catch (NullPointerException e) {
+			return "Happy";
 		}
 	}
 }
